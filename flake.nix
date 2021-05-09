0590e538f5640fa1f08d94f62d4d031f3e3a7038
Matthieu Coudron <mcoudron@hotmail.com>
2021-05-09 21:50:46 +0200

    feat: adding nix flake support
    so one can run `nix run github:berewt/REPLica` or get a devShell via
    `nix develop github:berewt/REPLica` (just `nix develop` in a clone).

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Golden tests for command-line interfaces.";
+
+  inputs.flake-utils.url = github:numtide/flake-utils;
+  inputs.idris = {
+    url = github:idris-lang/Idris2;
+    inputs.nixpkgs.follows = "nixpkgs";
+    inputs.flake-utils.follows = "flake-utils";
+  };
+
+  outputs = { self, nixpkgs, idris, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      npkgs = import nixpkgs { inherit system; };
+      idrisPkgs = idris.packages.${system};
+      buildIdris = idris.buildIdris.${system};
+      pkgs = buildIdris {
+        projectName = "replica";
+        src = ./.;
+        idrisLibraries = [];
+      };
+    in rec {
+      packages = pkgs // idrisPkgs;
+      defaultPackage = pkgs.build;
+      devShell = npkgs.mkShell {
+        buildInputs = [ idrisPkgs.idris2 npkgs.rlwrap ];
+        shellHook = ''
+          alias idris2="rlwrap -s 1000 idris2 --no-banner"
+        '';
+      };
+    }
+  );
+}
