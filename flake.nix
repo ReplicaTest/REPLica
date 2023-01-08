1d0a701fe941479913ab54440b5ab80696d3cf74
Nicolas Biri <nicolas@biri.name>
2023-01-08 11:41:39 +0100

    flake update

--- flake.nix
+++ flake.nix
@@ -1,36 +1,28 @@
 {
   description = "Golden tests for command-line interfaces.";
 
-  inputs.flake-utils.url = github:numtide/flake-utils;
-  inputs.idris = {
-    type = "github";
-    owner = "berewt";
-    repo = "Idris2";
-    ref = "flake-update";
-    inputs.nixpkgs.follows = "nixpkgs";
-    inputs.flake-utils.follows = "flake-utils";
-  };
-  inputs.papers = {
-    type = "github";
-    owner = "idris-lang";
-    repo = "Idris2";
-    dir = "libs/papers";
-    flake = false;
-  };
-  inputs.pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
-  inputs.replicadhall = {
-    type = "github";
-    owner = "ReplicaTest";
-    repo = "replica-dhall";
+  inputs = {
+    flake-utils.url = github:numtide/flake-utils;
+    idris = {
+      url = "github:idris-lang/Idris2";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+    papers = {
+      url = "github:idris-lang/Idris2?dir=libs/papers";
+      flake = false;
+    };
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    replicadhall.url = "github:ReplicaTest/replica-dhall";
   };
   outputs = { self, nixpkgs, idris, papers, flake-utils, pre-commit-hooks, replicadhall }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        npkgs = import nixpkgs { inherit system; };
-        inherit (npkgs)
+        pkgs = import nixpkgs { inherit system; };
+        inherit (pkgs)
           dhall
           zsh;
-        inherit (npkgs.haskellPackages)
+        inherit (pkgs.haskellPackages)
           dhall-json;
         idrisPkgs = idris.packages.${system};
         replica_dhall = replicadhall.packages.${system}.default;
@@ -70,7 +62,7 @@
           '';
         });
 
-        dockerImage = npkgs.dockerTools.buildImage {
+        dockerImage = pkgs.dockerTools.buildImage {
           name = "replica";
           config = {
             Cmd = [ "${replica}/bin/replica" ];
@@ -110,8 +102,8 @@
           };
         };
 
-        devShells.default = npkgs.mkShell {
-          packages = [ idrisPkgs.idris2 papersLib npkgs.rlwrap dhall dhall-json ];
+        devShells.default = pkgs.mkShell {
+          packages = [ idrisPkgs.idris2 papersLib pkgs.rlwrap dhall dhall-json ];
           shellHook = ''
             alias idris2="rlwrap -s 1000 idris2 --no-banner"
             ${self.checks.${system}.pre-commit-check.shellHook}
